21c3c075f59a7148db9c4d337fda63563b2f6e90
Musyasya <nashitik@mail.ru>
2022-12-13 15:29:58 +0300

    begin

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header =()=>{
+    return (
+        <header className='header'>
+            <img src='https://i.pinimg.com/originals/ae/7b/3e/ae7b3e027b890a6231d62d44ea6d939b.png' alt='#'/>
+        </header>
+    )
+}
+
+export default Header;
